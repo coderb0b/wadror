1828a161def0997b676f462433d115d89bda9b59
Engelbrekt <jari.silvo@cs.helsinki.fi>
2016-02-06 20:03:20 +0200

    v3

--- app/models/concerns/rating_average.rb
+++ app/models/concerns/rating_average.rb
@@ -1,6 +1,8 @@
 module RatingAverage
 	extend ActiveSupport::Concern
 	def average_rating
-		ratings.average(:score).round(2)
+		#ratings.average(:score).round(2)
+		return 0 if ratings.empty?
+		ratings.inject(0.0){ |sum, r| sum+r.score } / ratings.count
 	end
 end
